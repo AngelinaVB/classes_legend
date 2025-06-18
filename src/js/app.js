d6ca357f507abfcdc7962211d91f35a8a2b2153c
AngelinaVB <b.lina.1995@mail.ru>
2025-06-18 22:53:22 +0300

    two commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,59 +1,86 @@
 // TODO: write your code here
 export class Character {
-   constructor (name, type, health, level, attack, defence) 
-   {this.name = name;
+   constructor (name, type, health = 100, level = 1, attack, defence) 
+   {
+     const heroes = [
+            'Bowerman',
+            'Swordsman',
+            'Magician',
+            'Daemon',
+            'Undead',
+            'Zombie',
+        ];
+    
+    if (2 <= name.length && name.length <= 10) {
+        this.name = name;
+   }
+   else {throw new Error('Name is too short / long');
+   }
+   if (heroes.includes(type)) {
     this.type = type;
-    this.health = 100;
-    this.level = 1;
-    this.attack = attack;
-    this.defence = defence;
-
-    if (2 <= name.length <= 10) {
-        this.name;
-   }
-   else {throw new Error();
-   }
-   if (this.type === 'Bowerman' || 'Swordsman' || 'Magician' || 'Daemon' || 'Undead' || 'Zombie') {
-    this.type
    }
    else {
-    throw new Error();
+    throw new Error('Wrong type of Hero');
    }
+
+    this.name = name;
+    this.type = type;
+    this.health = health;
+    this.level = level;
+    this.attack = attack;
+    this.defence = defence;
    }
+
+   levelUp () {
+    if (this.health != 0) {
+   this.level = this.level + 1;
+   this.attack = (this.attack /100) * 20;
+   this.defence = (this.defence /100) * 20;
+   this.health = 100;
+   }
+   else {throw new Error ('It is impossible to level up a dead player')};
 }
 
+    damage (points) {
+        if (this.health >=0) {
+        this.health -= points * (1 - this.defence /100)
+    }
+}
+}
+
+
 export class Bowerman extends Character {
-    constructor(name, type, health, level, attack, defence) {
+    constructor(name) {
         super(name, 'Bowerman', health, level, 25, 25);
    }
 }
 
 export class Swordsman extends Character {
-    constructor(name, type, health, level, attack, defence) {
+    constructor(name) {
         super(name, 'Swordsman', health, level, 40, 10);
    }
 }
 
 export class Magician extends Character {
-    constructor(name, type, health, level, attack, defence) {
+    constructor(name) {
         super(name, 'Magician', health, level, 10, 40);
    }
 }
 
 export class Daemon extends Character {
-    constructor(name, type, health, level, attack, defence) {
+    constructor(name) {
         super(name, 'Daemon', health, level, 10, 40);
    }
 }
 
 export class Undead extends Character {
-    constructor(name, type, health, level, attack, defence) {
+    constructor(name) {
         super(name, 'Undead', health, level, 25, 25);
    }
 }
 
 export class Zombie extends Character {
-    constructor(name, type, health, level, attack, defence) {
+    constructor(name) {
         super(name, 'Zombie', health, level, 40, 10);
    }
 }
